fb490f3b23d5fa36fca983fff92a931ef4909586
Makarios-Tech101 <ogodogublessing1@gmail.com>
2023-10-26 21:09:40 +0100

    "EW-38 settings component removed"

--- src/layouts/dashboard/header.js
+++ src/layouts/dashboard/header.js
@@ -19,7 +19,7 @@ import { HEADER, NAV } from '../config-layout';
 import {
   // Searchbar,
   AccountPopover,
-  SettingsButton,
+  // SettingsButton,
   // LanguagePopover,
   // ContactsPopover,
   NotificationsPopover,
@@ -67,7 +67,7 @@ export default function Header({ onOpenNav }) {
 
         {/* <ContactsPopover /> */}
 
-        <SettingsButton />
+        {/* <SettingsButton /> */}
 
         <AccountPopover />
       </Stack>
